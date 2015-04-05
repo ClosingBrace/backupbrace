d89fe2b2b1aab9fc953e5a1c2b78ae166bc976cb
Hans Vredeveld <github@closingbrace.nl>
2015-04-05 21:13:28 +0200

    Add function to clone a directory tree
    The clone uses hard links for files.

--- src/usr/local/sbin/backup.py
+++ src/usr/local/sbin/backup.py
@@ -31,6 +31,7 @@ PROGRAM_VERSION = "0.1"
 import argparse
 import json
 import os
+import shutil
 import sys
 import textwrap
 import dateutil.parser
@@ -91,6 +92,73 @@ def decode_state_json(dct):
         dct['sets'] = {key : BackupSet.States.from_string(sets[key])
                 for key in sets}
     return dct
+
+
+def clone_tree(src, dst):
+    """Recursively clone a directory tree.
+
+    (Adapted from shutil.copytree. It differs from copytree in that it
+    also copies owner and group information and that it makes hard links
+    for files.)
+
+    The destination directory must not already exist. If exception(s)
+    occur, a BackupError is raised with a list of reasons.
+
+    Symbolic links in the source tree result in symbolic links in the
+    destination tree.
+
+    Args:
+        src (str) : The source directory to clone.
+        dst (str) : The destination to clone to. It must not yet exist.
+
+    Raises:
+        BackupError: When there were errors during cloning.
+    """
+    def copy_all_stat(src, dst, follow_symlinks=True):
+        """Copy all stat info (mode bits, atime, mtime, flags, uid, gid)
+        from src to dst.
+
+        If the optional flag `follow_symlinks` is not set, symlinks
+        aren't followed if and only if both `src` and `dst` are
+        symlinks.
+
+        Args:
+            src (str)             : The source to retrieve stats from.
+            dst (str)             : The destination to set stats on.
+            follow_symlinks (bool): If symlinks should be dereferenced.
+        """
+        stat = os.stat(src, follow_symlinks=follow_symlinks)
+        os.chown(dst, stat.st_uid, stat.st_gid, follow_symlinks=follow_symlinks)
+        shutil.copystat(src, dst, follow_symlinks=follow_symlinks)
+
+    names = os.listdir(src)
+
+    os.makedirs(dst)
+    errors = []
+    for name in names:
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+        try:
+            if os.path.islink(srcname):
+                linkto = os.readlink(srcname)
+                os.symlink(linkto, dstname)
+                copy_all_stat(srcname, dstname, follow_symlinks=False)
+            elif os.path.isdir(srcname):
+                clone_tree(srcname, dstname)
+            else:
+                os.link(srcname, dstname)
+        # catch the BackupError from the recursive clone_tree so that we
+        # can continue with other files
+        except BackupError as err:
+            errors.extend(err.args[0])
+        except OSError as why:
+            errors.append((srcname, dstname, str(why)))
+    try:
+        copy_all_stat(src, dst)
+    except OSError as why:
+        errors.append((src, dst, str(why)))
+    if errors:
+        raise BackupError(errors)
 
 
 class Environment:
