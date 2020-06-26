37bc48922ddd0b898dcf2af1574201df00341b33
Hans Vredeveld <github@closingbrace.nl>
2020-06-26 18:41:49 +0200

    Make the closingbrace directory a Python module

---
+++ closingbrace/__init__.py
@@ -0,0 +1,8 @@
+# Backupbrace
+# A script to create backups of a Linux system.
+#
+# Copyright (c) 2015-2020 Hans Vredeveld
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
